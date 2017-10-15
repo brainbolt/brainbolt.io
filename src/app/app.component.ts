afbaf66588dfbdac90e58e120e6e4ebd091de00b
brainbolt <brainbolts@gmail.com>
2017-10-15 13:57:36 -0700

    add cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { MatGridTile, MatCard, MatButton } from "@angular/material";
+import { MatCard, MatButton } from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,23 @@ import { MatGridTile, MatCard, MatButton } from "@angular/material";
 })
 export class AppComponent {
   title = 'brainbolt.io';
+  projects = [
+    {
+      title: "CubeNet",
+      imageUrl: "http://material.angular.io/assets/img/examples/shiba2.jpg",
+      imageAlt: "Photo of a Shiba Inu",
+      description: "Used object-oriented programming (OOP), relational databases, object-relational mapping (ORM), and web frameworks, to develop a comprehensive suite of mission-critical applications which increased productivity and profitability, and streamlined communications company-wide. Technologies used include C#, JavaScript, SQL Server, IIS, NHibernate, VBScript, and Bootstrap."
+    },
+    {
+      title: "wuweb",
+      imageUrl: "http://material.angular.io/assets/img/examples/shiba2.jpg",
+      imageAlt: "Photo of a Shiba Inu",
+      description: "Used object-oriented programming (OOP), relational databases, object-relational mapping (ORM), and web frameworks, to develop a comprehensive suite of mission-critical applications which increased productivity and profitability, and streamlined communications company-wide. Technologies used include C#, JavaScript, SQL Server, IIS, NHibernate, VBScript, and Bootstrap."
+    },
+    {
+      title: "wuiOS",
+      imageUrl: "http://material.angular.io/assets/img/examples/shiba2.jpg",
+      imageAlt: "Photo of a Shiba Inu",
+      description: "Used object-oriented programming (OOP), relational databases, object-relational mapping (ORM), and web frameworks, to develop a comprehensive suite of mission-critical applications which increased productivity and profitability, and streamlined communications company-wide. Technologies used include C#, JavaScript, SQL Server, IIS, NHibernate, VBScript, and Bootstrap."
+    }]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MatCardModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
