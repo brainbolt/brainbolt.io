ff8cee7ed74fcf45d16b61b71fdf1a4574bffac1
brainbolt <brainbolts@gmail.com>
2017-10-18 15:01:51 -0700

    create projects section, update descriptions

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -9,27 +9,19 @@ export class ProjectService {
     return [
       {
         title: "CubeNet",
-        subtitle:"Accounting package integration enabling task and order management",
+        subtitle:"Data-driven intranet application",
         imageUrl: "../../assets/images/cubenet-brainbolt-io.png",
         imageAlt: "CubeNet screenshot",
-        description: "Technologies used: SQL Server, IIS, ASP.Net, C#, Bootstrap, JQuery, and JavaScript",
+        description: ["Accounting package integration enabling task and order management.", "Technologies used include: SQL Server, IIS, ASP.Net, C#, NHibernate, Bootstrap, JQuery, and JavaScript"],
         url: "http://cubenet.brainbolt.io"
       },
       {
         title: "Valquest.com",
-        subtitle: "Company website",
+        subtitle: "Corporate Website",
         imageUrl: "../../assets/images/valquest-com.png",
         imageAlt: "Valquest.com screenshot",
-        description: "Used object-oriented programming (OOP), relational databases, object-relational mapping (ORM), and web frameworks, to develop a comprehensive suite of mission-critical applications which increased productivity and profitability, and streamlined communications company-wide. Technologies used include C#, JavaScript, SQL Server, IIS, NHibernate, VBScript, and Bootstrap.",
+        description: ["Technologies used include: ASP.Net MVC, JavaScript"],
         url: "http://www.valquest.com"
-      },
-      {
-        title: "wuiOS",
-        subtitle: "blah blah blah",
-        imageUrl: "http://material.angular.io/assets/img/examples/shiba2.jpg",
-        imageAlt: "Photo of a Shiba Inu",
-        description: "Used object-oriented programming (OOP), relational databases, object-relational mapping (ORM), and web frameworks, to develop a comprehensive suite of mission-critical applications which increased productivity and profitability, and streamlined communications company-wide. Technologies used include C#, JavaScript, SQL Server, IIS, NHibernate, VBScript, and Bootstrap.",
-        url: "http://cubenet.brainbolt.io"
       }];
  }  
 
